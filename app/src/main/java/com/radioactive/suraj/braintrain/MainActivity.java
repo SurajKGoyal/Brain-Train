ab8949ddf421f3cd6d0550b77a4957f98b15786d
SurajKGoyal <31234785+SurajKGoyal@users.noreply.github.com>
2017-10-01 11:17:17 +0530

    second commit

--- app/src/main/java/com/radioactive/suraj/braintrain/MainActivity.java
+++ app/src/main/java/com/radioactive/suraj/braintrain/MainActivity.java
@@ -156,6 +156,8 @@ public class MainActivity extends Activity {
                         }
                     }
                 }
+                answers.clear();
+                generateQuestion();
                 break;
         }
 
